2d8fdadcbb86e7aad71fa3c9bb75107b0a7f6bb9
luiscr918 <lc9107040@gmail.com>
2025-05-28 09:33:55 -0500

    clase 2 productos traspaso de controlador a la interfaz

--- src/main/java/com/itsqmet/controlador/ProductosController.java
+++ src/main/java/com/itsqmet/controlador/ProductosController.java
@@ -1,10 +1,16 @@
 package com.itsqmet.controlador;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+//@RequestMapping("/productos")
 public class ProductosController {
+    //llamar a las paginas
     @GetMapping ("/productos")
     public String catalogoProductos(){
         return "productos/catalogo";
@@ -12,5 +18,23 @@ public class ProductosController {
     @GetMapping("/registro-productos")
     public String registroProductos(){
         return "productos/registro";
+    }
+    @GetMapping("/saludo")
+    //enviar saludo personalizado a la pagina de catalogo
+    public String saludoPersonalizado(@RequestParam String nombre, Model model){
+        model.addAttribute("nombre",nombre);
+        return "productos/catalogo";
+    }
+    //Enviar Datos
+    @PostMapping("/enviar-producto")
+    public String guardarProductos(@RequestParam String id,
+                                   @RequestParam String nombre,
+                                   @RequestParam String precio,
+                                   @RequestParam String cantidad,Model model){
+        model.addAttribute("codigo",id);
+        model.addAttribute("nombre",nombre);
+        model.addAttribute("precio",precio);
+        model.addAttribute("cantidad",cantidad);
+        return "productos/catalogo";
     }
 }
