a0ef16e59f0b4d2eb1358c37b2ebfd4fc7127596
luiscr918 <lc9107040@gmail.com>
2025-05-28 20:42:17 -0500

    envio de datos de form de empleado a index exitoso

--- src/main/java/com/itsqmet/controlador/EmpleadoController.java
+++ src/main/java/com/itsqmet/controlador/EmpleadoController.java
@@ -1,8 +1,11 @@
 package com.itsqmet.controlador;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/empleado")
@@ -11,5 +14,19 @@ public class EmpleadoController {
     public String registroEmpleado(){
     return "empleados/registerEmpleado";
     }
-
+@PostMapping("/registrar-empleado")
+    public String guardarEmpleado(@RequestParam String nombre,
+                                  @RequestParam String apellido,
+                                  @RequestParam String direccion,
+                                  @RequestParam String fechaNac,
+                                  @RequestParam String intereses,
+                                  Model model
+                                  ){
+    model.addAttribute("nombre",nombre);
+    model.addAttribute("apellido",apellido);
+    model.addAttribute("direccion",direccion);
+    model.addAttribute("fechaNac",fechaNac);
+    model.addAttribute("intereses",intereses);
+    return "index";
+}
 }
